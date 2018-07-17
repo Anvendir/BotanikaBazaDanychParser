785acef6524492c2b6a701ca93afb896f04b1ad5
Rafał Kobak <rafalkobak@localhost.localdomain>
2018-07-17 19:18:44 +0200

    Fix some errors in ExcelDataBaseBuilder.py and adding BotanikaEtykiety.xlsx

--- ExcelDataBaseBuilder.py
+++ ExcelDataBaseBuilder.py
@@ -17,7 +17,6 @@ class ExcelDataBaseBuilder:
         for l_speciesRecord in p_formatedSpeciesList:
             l_singleCommand = self.__prepareSingleExcelDataBaseRecordForSingleSpeciesRecord(l_speciesRecord)
             l_outputFile.write(l_singleCommand)
-            l_outputFile.write(";")
             l_outputFile.write("\n")
         
         l_outputFile.close()
@@ -26,34 +25,34 @@ class ExcelDataBaseBuilder:
         print "When you import this file to MS Excel remember about coding type: \"65001 : Unicode (UTF-8)\"."
        
     def __prepareHeader(self):
-        l_header = "Nazwa łacinska;" + \
-                   "Nazwa polska;" + \
-                   "Nazwa łacinska + autorzy;" + \
-                   "Autor gatunku;" + \
-                   "Autor podgatunku;" + \
-                   "Synantropizacja;" + \
-                   "Podgatunek łacińska;" + \
-                   "Podgatunek polska;" + \
-                   "Gatunek łacińska;" + \
-                   "Gatunek polska;" + \
-                   "Rodzaj łacińska;" + \
-                   "Rodzaj polska;" + \
-                   "Rodzina łacińska;" + \
-                   "Rodzina polska;" + \
-                   "Rząd łacińska;" + \
-                   "Rząd polska;" + \
-                   "Nadrząd łacińska;" + \
-                   "Nadrząd polska;" + \
-                   "Podklasa łacińska;" + \
-                   "Podklasa polska;" + \
-                   "Klasa łacińska;" + \
-                   "Klasa polska;" + \
-                   "Podgromada łacińska;" + \
-                   "Podgromada polska;" + \
-                   "Gromada łacińska;" + \
-                   "Gromada polska;" + \
-                   "Królestwo łacińska;" + \
-                   "Królestwo polska;"
+        l_header = "Nazwa polska (1);" + \
+                   "Nazwa łacinska (2);" + \
+                   "Nazwa łacinska + autorzy (3);" + \
+                   "Autor gatunku (4);" + \
+                   "Autor podgatunku (5);" + \
+                   "Synantropizacja (6);" + \
+                   "Podgatunek polska (7);" + \
+                   "Podgatunek łacińska (8);" + \
+                   "Gatunek polska (9);" + \
+                   "Gatunek łacińska (10);" + \
+                   "Rodzaj polska (11);" + \
+                   "Rodzaj łacińska (12);" + \
+                   "Rodzina polska (13);" + \
+                   "Rodzina łacińska (14);" + \
+                   "Rząd polska (15);" + \
+                   "Rząd łacińska (16);" + \
+                   "Nadrząd polska (17);" + \
+                   "Nadrząd łacińska (18);" + \
+                   "Podklasa polska (19);" + \
+                   "Podklasa łacińska (20);" + \
+                   "Klasa polska (21);" + \
+                   "Klasa łacińska (22);" + \
+                   "Podgromada polska (23);" + \
+                   "Podgromada łacińska (24);" + \
+                   "Gromada polska (25);" + \
+                   "Gromada łacińska (26);" + \
+                   "Królestwo polska (27);" + \
+                   "Królestwo łacińska (28);"
         return l_header;
 
     def __prepareSingleExcelDataBaseRecordForSingleSpeciesRecord(self, p_speciesRecord):
@@ -68,8 +67,8 @@ class ExcelDataBaseBuilder:
 
         l_systemathicHierarchy = self.__prepareSystematicHierarchy(p_speciesRecord)
 
-        l_valuesFromAllFields = l_latinName + ";" + \
-                                l_polishName + ";" + \
+        l_valuesFromAllFields = l_polishName + ";" + \
+                                l_latinName + ";" + \
                                 l_latinNamePlusAuthor + ";" + \
                                 l_author + ";" + \
                                 l_authorSubsp + ";" + \
@@ -81,51 +80,51 @@ class ExcelDataBaseBuilder:
     def __prepareSystematicHierarchy(self, p_speciesRecord):
         l_dataExtractor = SingleRecordDataExtractor() 
 
-        l_subSpeciesLatin = l_dataExtractor.getLatinSubspeciesName(p_speciesRecord)
         l_subSpeciesPolish = l_dataExtractor.getPolishSubspeciesName(p_speciesRecord)
-        l_speciesLatin = l_dataExtractor.getLatinSpeciesName(p_speciesRecord)
+        l_subSpeciesLatin = l_dataExtractor.getLatinSubspeciesName(p_speciesRecord)
         l_speciesPolish = l_dataExtractor.getPolishSpeciesName(p_speciesRecord)
-        l_genusLatin = l_dataExtractor.getLatinGenusName(p_speciesRecord)
+        l_speciesLatin = l_dataExtractor.getLatinSpeciesName(p_speciesRecord)
         l_genusPolish = l_dataExtractor.getPolishGenusName(p_speciesRecord)
-        l_family = l_dataExtractor.getLatinFamilyName(p_speciesRecord)
+        l_genusLatin = l_dataExtractor.getLatinGenusName(p_speciesRecord)
         l_familyPolish = ""
-        l_order = l_dataExtractor.getLatinOrderName(p_speciesRecord)
+        l_family = l_dataExtractor.getLatinFamilyName(p_speciesRecord)
         l_orderPolish = ""
-        l_superorder = l_dataExtractor.getLatinSuperorderName(p_speciesRecord)
+        l_order = l_dataExtractor.getLatinOrderName(p_speciesRecord)
         l_superorderPolish = "" 
-        l_subclass = l_dataExtractor.getLatinSubclassName(p_speciesRecord)
+        l_superorder = l_dataExtractor.getLatinSuperorderName(p_speciesRecord)
         l_subclassPolish = ""
-        l_class_ = l_dataExtractor.getLatinClassName(p_speciesRecord)
+        l_subclass = l_dataExtractor.getLatinSubclassName(p_speciesRecord)
         l_classPolish = ""
-        l_subdivision = l_dataExtractor.getLatinSubdivisionName(p_speciesRecord)
+        l_class_ = l_dataExtractor.getLatinClassName(p_speciesRecord)
         l_subdivisionPolish = ""
-        l_division = l_dataExtractor.getLatinDivisionName(p_speciesRecord)
+        l_subdivision = l_dataExtractor.getLatinSubdivisionName(p_speciesRecord)
         l_divisionPolish = ""
-        l_realm = ""
+        l_division = l_dataExtractor.getLatinDivisionName(p_speciesRecord)
         l_realmPolish = ""
+        l_realm = ""
 
-        l_systemathicHierarchy = l_subSpeciesLatin + ";" + \
-                                 l_subSpeciesPolish + ";" + \
-                                 l_speciesLatin + ";" + \
+        l_systemathicHierarchy = l_subSpeciesPolish + ";" + \
+                                 l_subSpeciesLatin + ";" + \
                                  l_speciesPolish + ";" + \
-                                 l_genusLatin + ";" + \
+                                 l_speciesLatin + ";" + \
                                  l_genusPolish + ";" + \
-                                 l_family + ";" + \
+                                 l_genusLatin + ";" + \
                                  l_familyPolish + ";" + \
-                                 l_order + ";" + \
+                                 l_family + ";" + \
                                  l_orderPolish + ";" + \
-                                 l_superorder + ";" + \
+                                 l_order + ";" + \
                                  l_superorderPolish + ";" + \
-                                 l_subclass + ";" + \
+                                 l_superorder + ";" + \
                                  l_subclassPolish + ";" + \
-                                 l_class_ + ";" + \
+                                 l_subclass + ";" + \
                                  l_classPolish + ";" + \
-                                 l_subdivision + ";" + \
+                                 l_class_ + ";" + \
                                  l_subdivisionPolish + ";" + \
-                                 l_division + ";" + \
+                                 l_subdivision + ";" + \
                                  l_divisionPolish + ";" + \
-                                 l_realm + ";" + \
-                                 l_realmPolish
+                                 l_division + ";" + \
+                                 l_realmPolish + ";" + \
+                                 l_realm + ";"
 
         return l_systemathicHierarchy 
 
